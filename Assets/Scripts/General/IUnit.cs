32ed48804725c4c0bdd3c3d448328c9e8dbe7040
Eugene <eugene@hazeluff.com>
2018-05-01 15:55:56 -0700

    Add some comments

--- Assets/Scripts/General/IUnit.cs
+++ Assets/Scripts/General/IUnit.cs
@@ -10,10 +10,10 @@
     // Base Stats
     int HP { get; }
     int MP { get; }
-    int Atk { get; }
-    int Def { get; }
-    int Int { get; }
-    int Res { get; }
+    int Atk { get; } // Double Atk over Def => Ignore Def
+    int Def { get; } // Double Def over Atk => 0 dmg
+    int Int { get; } // Double Int over Res => Ignore Res
+    int Res { get; } // Double Res over Int => 0 dmg
     int Hit { get; } // Double Hit stat vs => Crit
     int Spd { get; } // Double Speed stat vs => Double Attack
 
